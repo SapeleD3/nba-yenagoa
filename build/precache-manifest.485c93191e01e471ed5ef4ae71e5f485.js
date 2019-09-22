7e8968f7a82e30bebe3eb0a88bfafdf6edac1573
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-09-22 19:59:20 +0100

    fix deploy

---
+++ build/precache-manifest.485c93191e01e471ed5ef4ae71e5f485.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c245018dfe305dcf20fdc552ebe94a",
+    "url": "/nba-yenagoa/index.html"
+  },
+  {
+    "revision": "b2053dd7f416c4b37cf3",
+    "url": "/nba-yenagoa/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "bb799fa84ecac6f28249",
+    "url": "/nba-yenagoa/static/css/main.e7819147.chunk.css"
+  },
+  {
+    "revision": "b2053dd7f416c4b37cf3",
+    "url": "/nba-yenagoa/static/js/2.a293740a.chunk.js"
+  },
+  {
+    "revision": "bb799fa84ecac6f28249",
+    "url": "/nba-yenagoa/static/js/main.680396dc.chunk.js"
+  },
+  {
+    "revision": "95e8d138922aefc2e72e",
+    "url": "/nba-yenagoa/static/js/runtime~main.ec09972d.js"
+  },
+  {
+    "revision": "edf4b2115ff6242cc899a987d3f2bb59",
+    "url": "/nba-yenagoa/static/media/black-lawyer.edf4b211.jpg"
+  },
+  {
+    "revision": "bf43a1c29c3b8bdcb37a98d5736cea8e",
+    "url": "/nba-yenagoa/static/media/logo.bf43a1c2.png"
+  }
+]);
